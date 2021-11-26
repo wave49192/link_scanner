3bacc3eaf576d544e974384156293e17ec1d00ae
wave49192 <sirawich.tu@ku.th>
2021-11-26 19:56:48 +0700

    add more codes in main

--- link_scan.py
+++ link_scan.py
@@ -3,7 +3,7 @@ from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import urllib
+import urllib.error, urllib.request
 import sys
 
 
@@ -21,11 +21,14 @@ def get_links(url):
         url = link.get_attribute('href')
         if url != None:
             if '#' in url:
-                all_list.append(url.split('#')[0])
+                if url.split('#')[0] not in all_list:
+                    all_list.append(url.split('#')[0])
             elif '?' in url:
-                all_list.append(url.split('?')[0])
+                if url.split('?')[0] not in all_list:
+                    all_list.append(url.split('?')[0])
             else:
-                all_list.append(url)
+                if url not in all_list:
+                    all_list.append(url)
     return all_list
 
 
@@ -47,15 +50,25 @@ def is_valid_url(url: str) -> bool:
 def invalid_urls(urllist: List[str]) -> List[str]:
     """Validate the urls in urllist and return a new list containing
     the invalid or unreachable urls.
+    Returns:
+        A new list containing only the invalid or unreachable URLs from the parameter list.
     """
-    inavlid_url_list = []
+    new_list = []
     for url in urllist:
-        if is_valid_url(url) == False:
-            inavlid_url_list.append(url)
-    return inavlid_url_list
+        if is_valid_url == False:
+            new_list.append(url)
+    return new_list
 
 
 if __name__ == "__main__":
     browser: WebDriver = webdriver.Chrome('C:/Program Files (x86)/chromedriver.exe')
     url = sys.argv[1]
     link_list = get_links(url)
+    bad_link_list = invalid_urls(link_list)
+    print('This is good link')
+    for url in link_list:
+        print(url)
+    print('This is bad link')
+    for url in bad_link_list:
+        print(url)
+    browser.quit()
